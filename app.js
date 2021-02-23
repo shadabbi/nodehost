b8d1b578939af557f49564a9b895129df3b538ce
Shadab <shadab.ali7503@gmail.com>
2021-02-23 13:54:36 +0530

    pre-heroku test

--- app.js
+++ app.js
@@ -65,8 +65,8 @@ app.use((req, res, next) => {
 mongoose
   .connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then((result) => {
-    app.listen(3000);
-    // app.listen(process.env.PORT);
+    // app.listen(3000);
+    app.listen(process.env.PORT);
   })
   .catch((err) => {
     console.log(err);
