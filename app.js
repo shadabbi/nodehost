60514ea91d0f3449e4b25059ad81313cea74bf41
Shadab <shadab.ali7503@gmail.com>
2021-02-23 13:40:05 +0530

    pre-heroku

--- app.js
+++ app.js
@@ -66,9 +66,8 @@ mongoose
   .connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then((result) => {
     app.listen(3000);
+    // app.listen(process.env.PORT);
   })
   .catch((err) => {
     console.log(err);
   });
-
-// app.listen(process.env.PORT);
